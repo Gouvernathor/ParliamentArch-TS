55e2967f4c76fb458e19749aaa758fbb8859ddc1
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-09-04 07:01:03 +0200

    Simplify the main function's interface

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,29 @@ import { dispatchSeats, getGroupedSVG, type GetGroupedSVGOptions, type SeatData,
 
 export { type SeatData, type SeatDataWithNumber } from "./svg.js";
 
+interface GetSVGFromAttributionOptions extends GetSeatsCentersOptions, GetGroupedSVGOptions {
+    seatRadiusFactor: number;
+}
+
+/**
+ * This is the preferred overload.
+ */
 export function getSVGFromAttribution(
     attribution: Map<SeatData, number> | readonly SeatDataWithNumber[],
-    seatRadiusFactor: number = .8,
+    options?: Partial<GetSVGFromAttributionOptions>,
+): SVGSVGElement;
+/**
+ * This version is deprecated. Please use the overload with the single options object instead.
+ */
+export function getSVGFromAttribution(
+    attribution: Map<SeatData, number> | readonly SeatDataWithNumber[],
+    seatRadiusFactor?: number,
+    getSeatsCentersOptions?: Partial<GetSeatsCentersOptions>,
+    getGroupedSVGOptions?: Partial<GetGroupedSVGOptions>,
+): SVGSVGElement;
+export function getSVGFromAttribution(
+    attribution: Map<SeatData, number> | readonly SeatDataWithNumber[],
+    options: number|Partial<GetSVGFromAttributionOptions> = {},
     getSeatsCentersOptions: Partial<GetSeatsCentersOptions> = {},
     getGroupedSVGOptions: Partial<GetGroupedSVGOptions> = {},
 ): SVGSVGElement {
@@ -13,10 +33,15 @@ export function getSVGFromAttribution(
         attribution = new Map(attribution.map(seatData => [seatData, seatData.nSeats ?? 1]));
     }
 
+    if (typeof options === "number") {
+        options = { seatRadiusFactor: options, ...getSeatsCentersOptions, ...getGroupedSVGOptions };
+    }
+    options.seatRadiusFactor ??= .8;
+
     const nSeats = [...attribution.values()].reduce((a, b) => a + b, 0);
 
-    const results = getSeatsCenters(nSeats, getSeatsCentersOptions);
+    const results = getSeatsCenters(nSeats, options);
     const seatCentersByGroup = dispatchSeats(attribution, [...results.keys()].sort((a, b) => results.get(b)! - results.get(a)!));
-    const seatActualRadius = seatRadiusFactor * getRowThickness(getNRowsFromNSeats(nSeats));
-    return getGroupedSVG(seatCentersByGroup, seatActualRadius, getGroupedSVGOptions);
+    const seatActualRadius = options.seatRadiusFactor * getRowThickness(getNRowsFromNSeats(nSeats));
+    return getGroupedSVG(seatCentersByGroup, seatActualRadius, options);
 }
