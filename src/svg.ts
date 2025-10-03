2f18d6dc427209eb210acf9bbfec84e1cd3e6057
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-10-04 00:24:43 +0200

    Better handling, actually optionally, with a different policy

--- src/svg.ts
+++ src/svg.ts
@@ -14,8 +14,10 @@ export interface SeatDataWithNumber extends SeatData {
  * without ever importing it in browser mode.
  */
 if (!globalThis.document) {
-    globalThis.document = (await import("jsdom")
-        .then(m => new m.JSDOM().window.document));
+    await import("jsdom")
+        .then(m => globalThis.document = new m.JSDOM().window.document)
+        .catch(() =>
+            console.error("Failed to load jsdom or the document constant at ParliamentArch load time : you need to set globalThis.document before generating SVGs in Node.js"));
 }
 
 /**
