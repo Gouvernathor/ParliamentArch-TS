bc06f351603f05b212af6038771b81769a847055
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-10-04 16:00:23 +0200

    Warn instead of error

--- src/svg.ts
+++ src/svg.ts
@@ -17,7 +17,7 @@ if (!globalThis.document) {
     await import("jsdom")
         .then(m => globalThis.document = new m.JSDOM().window.document)
         .catch(() =>
-            console.error("Failed to load jsdom or the document constant at ParliamentArch load time : you need to set globalThis.document before generating SVGs in Node.js"));
+            console.warn("Failed to load jsdom or the document constant at ParliamentArch load time : you need to set globalThis.document before generating SVGs in Node.js"));
 }
 
 /**
--- src/westminster/svg.ts
+++ src/westminster/svg.ts
@@ -8,7 +8,7 @@ if (!globalThis.document) {
     await import("jsdom")
         .then(m => globalThis.document = new m.JSDOM().window.document)
         .catch(() =>
-            console.error("Failed to load jsdom or the document constant at ParliamentArch load time : you need to set globalThis.document before generating SVGs in Node.js"));
+            console.warn("Failed to load jsdom or the document constant at ParliamentArch load time : you need to set globalThis.document before generating SVGs in Node.js"));
 }
 
 const SVG_NS = "http://www.w3.org/2000/svg";
