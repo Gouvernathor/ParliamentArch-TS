7e2d8f0d2a1ce91028151c23e09988e6fb870b48
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-03-25 01:48:57 +0100

    remove direct dependency on the legacy module

--- src/svg.ts
+++ src/svg.ts
@@ -13,17 +13,6 @@ export type SeatDataWithNumber = SeatData & {
 }
 
 /**
- * Makes the document constant available, whether in a browser or in Node.js,
- * without ever importing it in browser mode.
- */
-if (!globalThis.document) {
-    await import("jsdom")
-        .then(m => globalThis.document = new m.JSDOM().window.document)
-        .catch(() =>
-            console.error("Failed to load jsdom or the document constant at ParliamentArch load time : you need to set globalThis.document before generating SVGs in Node.js"));
-}
-
-/**
  * Typically S is a tuple of x/y coordinates.
  * Typically the groups are ordered from the left to the right, and the seats are ordered from left to right.
  * If too few or too many seats are provided, an error is thrown.
