0a563844cfc4c7e4a9be97f9d095b4af4654141b
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-04-22 02:20:55 +0200

    fix ignored error

--- packages/svg/src/document-loader.ts
+++ packages/svg/src/document-loader.ts
@@ -3,9 +3,11 @@
  * without ever importing it in browser mode.
  */
 if (!globalThis.document) {
-    //@ts-ignore
+    // @ts-ignore
     await import("jsdom")
         .then(m => globalThis.document = new m.JSDOM().window.document)
         .catch(() =>
             console.error("Failed to load jsdom or the document constant at ParliamentArch load time : you need to set globalThis.document before generating SVGs in Node.js"));
 }
+
+export {};
