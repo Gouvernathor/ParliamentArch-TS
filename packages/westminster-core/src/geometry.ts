7d23481ac3a0210ae829ef8279f74f9b23a4dbe9
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-05-24 17:18:37 +0200

    cut up the geometry part into submodules

--- packages/westminster-core/src/geometry.ts
+++ packages/westminster-core/src/geometry.ts
@@ -1,4 +1,9 @@
 import { Area, CoordinatesPerParty, CoordinatesPerPartyPerArea, areaRecord } from "./common.js";
+import { defaultOptions, Options } from "./geometry/common.js";
+
+// TODO move/rename as geometry/index.ts and update package.json
+
+export { Options as GetSeatCoordinatesPerAreaOptions };
 
 /**
  * Number of seats for each party for each area.
@@ -20,53 +25,6 @@ type NSeatsPerArea = { readonly [a in Area]: number };
  * Number of rows and columns for each area.
  */
 type NRowsAndColsPerArea = { readonly [a in Area]: { readonly nRows: number; readonly nCols: number } };
-
-
-interface Options {
-    /**
-     * The number of rows for each of the two wings.
-     * Ignored if 0, invalid if negative.
-     * If ignored, the actual number of rows is computed automatically.
-     */
-    wingNRows: number; // default 0
-
-    /**
-     * The number of columns for the crossbenchers.
-     * Ignored if 0, invalid if negative,
-     * will also be ignored if inferior to the total number of crossbenchers.
-     * Previously called centerCols.
-     * If ignored, the actual number of columns is computed automatically.
-     */
-    crossNCols: number; // default 0
-
-    /**
-     * Whether parties of the same wing are allowed to share the same column,
-     * or the same row for crossbenchers.
-     *
-     * Formerly called "cozy".
-     */
-    packed: boolean; // default true
-
-    /**
-     * Whether to prioritize having equal columns between the two wings,
-     * over having equal rows.
-     */
-    // fullWidth: boolean; // default false
-}
-export { Options as GetSeatCoordinatesPerAreaOptions };
-function defaultOptions({
-    wingNRows = 0,
-    crossNCols = 0,
-    packed = true,
-    // fullWidth = false,
-}: Partial<Readonly<Options>> = {}): Options {
-    return {
-        wingNRows,
-        crossNCols,
-        packed,
-        // fullWidth,
-    };
-}
 
 /*
 Disposition and rules:
